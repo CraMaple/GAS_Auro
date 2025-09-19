9ebb56b461f09a8d87fe86a75f92b10427503a31
CraMaple <309528561@qq.com>
2025-09-19 10:45:26 +0800

    Initial Project Files

---
+++ Source/Auro/Auro.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Auro.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Auro, "Auro" );
---
+++ Source/Auro/AuroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURO_API AAuroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
